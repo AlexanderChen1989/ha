9a94ac079d2d3c64ce36b8bda89db047540e0d07
Aaron Halo <aaronhalo@AlexanderdeMacBook-Pro.local>
2015-12-19 16:34:43 +0800

    add more comments

--- example/main.go
+++ example/main.go
@@ -10,19 +10,31 @@ import (
 )
 
 func main() {
+	// a function that shoud be watched
 	fn := func() {
 		var i *int
 		*i = 100
 	}
+
+	// when fn stop
 	onStop := ha.OnStop(func(err error) {
 		fmt.Println(err)
 	})
+
+	// try to restart fn at most 5 times
 	max := ha.Max(5)
+
+	// wait sometime before restart fn
 	wait := ha.Wait(1 * time.Second)
+
+	// context to stop restarting fn
 	ctx, cancel := context.WithCancel(context.Background())
+
 	go func() {
+		// sleep 2 seconds
 		time.Sleep(2 * time.Second)
 		cancel()
 	}()
+
 	ha.Watch(fn, onStop, wait, max, ha.Context(ctx))
 }
